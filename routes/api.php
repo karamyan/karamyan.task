ed34b38ef0bbfaef49d02807c75544f8a7e60945
Levon Karamyan <lyov.karamyan@gmail.com>
2021-07-14 04:07:07 +0400

    add sanctum api authentication

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post("/token/create", 'AuthController@login')->name('create_token');
+
+//All secure URL's
+Route::group(['middleware' => 'auth:sanctum'], function () {
 });
